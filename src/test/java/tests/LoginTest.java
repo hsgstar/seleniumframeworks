8a07cde870de5bc05c56deaa51a20b8bfc216e0e
hsgstar <himadrisenguptabecse@gmail.com>
2026-04-29 19:06:23 +0530

    seleniumframework

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest
+{
+	@Test
+	public void testValidation()
+	{
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.enterUsername("admin@yourstore.com");
+		loginPage.enterPassword("admin");
+		loginPage.clickLogin();
+		System.out.println("Title is" +driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+	}
+}
